a9ca2582da462b47646b9f78d2b780cd62519a5b
Damian <daamain@gmail.com>
2013-11-24 20:29:47 +0100

    CN0 Total policzone

--- sts.m
+++ sts.m
@@ -103,3 +103,26 @@ CN0_total_lin = 10^(CN0_total/10)
 CN0_up_lin = 10^(CN0_up/10)
 
 CN0_down = 10*log10(1/(CN0_total_lin^-1 - CN0_up_lin^-1))
+
+GTes = CN0_down - bilans_down
+
+c = 299792458
+
+GT_a = 10 * log10((pi^2 * D^2 * n)/((c/f_up)^2))
+
+T = GT_a - GT
+
+GT_b = GTes + T
+
+D_b = sqrt(((10^(GT_b/10)) * (c/f_down)^2)/(n*pi^2))
+
+
+CN0_b_up = liczBilans(lng_b,lat_b, lng_sat, P0_b, D_b, n, f_down, La_b, h_b, gammaR_b, dost, GT_sat, 0, 0)
+
+CN0_a_down = liczBilans(lng_a,lat_a, lng_sat, P0_a, D, n, f_up, La, h_a, gammaR, dost, 0, EIRP_sat, OBO) + GT
+
+CN0_b_up_lin = 10^(CN0_b_up/10)
+
+CN0_a_down_lin = 10^(CN0_a_down/10)
+
+CN0_total_a = 10*log10(1/(CN0_b_up_lin^-1 + CN0_a_down_lin^-1))
